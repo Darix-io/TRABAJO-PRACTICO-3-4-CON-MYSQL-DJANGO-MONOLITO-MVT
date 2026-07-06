94a715eb9e5998fa5a30a5c4d78c1e8b58c3fc0b
Darix-io <rmoraj3@unemi.edu.ec>
2026-07-06 02:47:04 -0500

    Laboratorio-1-y-2

---
+++ backend/config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+from security.views import InicioTemplate
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", InicioTemplate.as_view(), name="home"),
+    path("security/", include("security.urls")),
+]
