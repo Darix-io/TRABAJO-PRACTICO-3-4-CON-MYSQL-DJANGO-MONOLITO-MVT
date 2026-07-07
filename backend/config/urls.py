50eb6332dd4a1ef35409037b28c4f80db4acda79
Darix-io <rmoraj3@unemi.edu.ec>
2026-07-06 23:52:25 -0500

    CRUD Catalogo categoria mas productos

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from security.views import InicioTemplate, LoginPageView
@@ -7,4 +9,8 @@ urlpatterns = [
     path("", InicioTemplate.as_view(), name="home"),
     path("login/", LoginPageView.as_view(), name="login"),
     path("security/", include("security.urls")),
+    path("catalog/", include("catalog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
