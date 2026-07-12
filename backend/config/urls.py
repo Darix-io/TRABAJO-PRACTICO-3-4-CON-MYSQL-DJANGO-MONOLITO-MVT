77f50df46754f215fdfbd67e48bb24a264fabcff
Darix-io <rmoraj3@unemi.edu.ec>
2026-07-12 17:45:28 -0500

    Dashboard con Chart.js

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from core.views import DashboardView
 from security.views import InicioTemplate, LoginPageView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", InicioTemplate.as_view(), name="home"),
+    path("", DashboardView.as_view(), name="home"),
     path("login/", LoginPageView.as_view(), name="login"),
     path("security/", include("security.urls")),
     path("catalog/", include("catalog.urls")),
